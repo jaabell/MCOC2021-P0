20c30c64e068fea177e298dd5a66068b62ac00d8
MatiasCornejo <62267612+MatiasCornejo@users.noreply.github.com>
2021-09-03 22:54:01 -0400

    Entrega6

---
+++ Entrega 6/MatrizDispersa(solve).py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 20 08:58:09 2021
+@author: matia
+"""
+
+from time import perf_counter 
+from numpy import zeros, half, single , double , longdouble, eye ,ones
+import scipy.sparse.linalg as lin
+from scipy.linalg import inv ,solve ,eigh
+from scipy import matmul
+from scipy import linalg
+
+def laplaciana(N,dtype):
+    A = zeros((N,N),dtype=dtype)
+    
+    for i in range(N):
+        A[i,i]=2
+      
+        for j in range(max(0,i-2),i):
+            if abs(i-j)==1:
+                   A[i,i]=-1
+                   A[j,i]=-1               
+    return A
+
+ListNs=[2,5,10,15,20,25,50,100,200,500,1000,2000,5000,10000,20000,30000,35000]
+#
+ListDtsInversion=[]
+ListDtsEnsamblaje=[]
+
+fid = open("MatrizDispersa,double(solve).txt","w") #vario el nombre dependendiendo del caso para lograr los archivos txt
+time=1
+while time<=10:
+    print(time)
+    time+=1
+    
+    for N in ListNs:    
+     
+        
+        t1 = perf_counter()
+            
+        a=laplaciana(N,dtype=double) #dependiendo el caso lo cambio para float o double
+        b=ones(N)
+       # A=sparse.csr_matrix(a)
+        #B=sparse.csr_matrix(b)
+        t2 = perf_counter()
+        #print(A)
+        #am1= inv(a)
+        
+        mul=lin.spsolve(a,b) #este solamente lo ocupamos para caso matriz llena
+        
+    
+        t3 = perf_counter()
+        
+        dt_ensamblaje = t2-t1
+        
+        dt_inversion = t3-t2
+        
+        #bytes_total= a.nbytes + am1.nbytes
+        
+        #print(f"Uso memoria:{bytes_total}s ")
+        
+       # print(f"Tiempo ensamblaje: {dt_ensamblaje}s")
+        
+        #print(f"Tiempo inversion: {dt_inversion}s")
+
+         
+         
+        #UsoMemoriaTeorico=4*N*N
+        #UsoMemoriaNumpy=a.nbytes
+          
+         
+        #UsoMemoriaTotal=bytes_total
+             
+        
+        ListDtsEnsamblaje.append(dt_ensamblaje)
+        ListDtsInversion.append(dt_inversion)
+       # ListUsoMemoria.append(UsoMemoriaTotal)
+        # print( UsoMemoriaTotal)
+       
+        print(f"N={N} dt_ensamblaje={dt_ensamblaje} dt_inversion={dt_inversion} ")
+        fid.write(f"{N} {dt_ensamblaje} {dt_inversion}\n ")
+     
+    
+     
+            
+fid.close()
